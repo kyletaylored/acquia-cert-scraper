b26568123aa5b1d3d72794804017ff12f34eee5a
Kyle Taylor <kyletaylored@gmail.com>
2019-09-03 16:12:28 -0500

    Get both pubsub arguments

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import csv
 import requests
 import time
 import os
+import base64
 
 
 class BigQuery:
@@ -374,7 +375,28 @@ def results(request):
         return jsonify(records)
 
 
-def crawl_records(data):
+def crawl_records(event, context):
+    """Background Cloud Function to be triggered by Pub/Sub.
+    Args:
+         event (dict):  The dictionary with data specific to this type of
+         event. The `data` field contains the PubsubMessage message. The
+         `attributes` field will contain custom attributes if there are any.
+         context (google.cloud.functions.Context): The Cloud Functions event
+         metadata. The `event_id` field contains the Pub/Sub message ID. The
+         `timestamp` field contains the publish time.
+    """
+    ps = Pubsub()
+
+    print("""This Function was triggered by messageId {} published at {}
+    """.format(context.event_id, context.timestamp))
+
+    if 'data' in event:
+        data = base64.b64decode(event['data'])
+        name = ps.decode(data)
+        print(name)
+
+    print(event)
+
     """
     Crawl Acquia certifications.
     """
